8adbae2a8c5d1bcd5d3b01a00914eda030a35aac
rnZolo <rjsagge@gmail.com>
2023-04-21 10:38:45 +0800

    still trying

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const math = require('mathjs');
 
 
 // elements in simple calculator
@@ -5,6 +6,7 @@ const inputs = document.querySelectorAll('.input'),
     devInfoBtn = document.querySelector('.info-btn'),
     delClrBtns = document.querySelectorAll('.erase'),
     dynamicDisplay = document.querySelectorAll('.dynamic'),
+    formulaDisplay = document.querySelector('.formula'),
     inputValue = document.querySelector('.val'),
     inputOperatorDisplay = document.querySelector('.operator'),
     changers = document.querySelectorAll('.change'),
@@ -50,61 +52,47 @@ operators.forEach((op) => {
     })
 })
 
-// changers.forEach((changer, index) => {
-//     if (index === 0) {
-//         console.log(changer.value)
-//     } else if (index === 1) {
-//         console.log(changer.value)
-//     } else {
-//         // percent()
-//         console.log(changer.value)
-//     }
-// })
-
-
 function equals() {
-    let toBeCal = staging
-    console.log(toBeCal);
-    toBeCal.pop()
-    console.log(toBeCal);
-    let output = toBeCal.join(' ');
-    let result = math.eval(output);
-    console.log(result);
+    let staged = staging
+    console.log(staged);
+    let output = staged.join(' ');
+    const parser = math.parser()
+    let result = parser.evaluate(output);
+    staging.push(result)
     inputValue.textContent = result;
 }
 
+
 function addOperator(op, orig) {
-    if (!inputOperatorDisplay.textContent) { //if there's no operator allow only one
-        inputOperatorDisplay.textContent = op
-    }
-    let fval = inputAsArray.join('')
-    if (inputValue.textContent) {
-        addToExp(fval)
-        addToExp(orig)
+    //avoid adding operator if no initial value
+    if (inputValue.textContent) { //true, if not empty 
+        let fval = inputAsArray.join('') //pass the value of inputs as string
+
+        console.log(`added to stage: ${staging}`)
+        if (!inputOperatorDisplay.textContent) { //if there's no operator allow only one
+            inputOperatorDisplay.textContent = op
+            staged(fval) //add input into stage
+            staged(orig) //add input into stage
+        }
+        formulaDisplay.textContent += `${staging} `
         inputAsArray = []
         inputValue.textContent = ''
-        console.log(inputOperatorDisplay.length)
-    } else {
-        inputAsArray = []
-        inputValue.textContent = ''
-        console.log(`staged: ${staging}`)
+        inputOperatorDisplay.textContent = ''
     }
-    inputAsArray = []
-    inputValue.textContent = ''
-    console.log(`staged: ${staging}`)
 }
 
-function addToExp(exp) {
+function staged(exp) {
     staging.push(exp)
+    formulaDisplay.textContent += exp
     console.log(staging.length)
-    if (staging.length > 3) {
+    if (staging.length >= 3) {
         equals()
     }
 }
 
 function pushInput(value) {
     checkDevInfo()
-    // inputValue.textContent += value
+    inputValue.textContent += value
     inputAsArray.push(value)
     console.log(`cliked: ${inputAsArray}`)
 }
@@ -168,3 +156,13 @@ function displayNone(element) { //hide an element
     element.style.display = "none"
 }
 
+    // changers.forEach((changer, index) => {
+    //     if (index === 0) {
+    //         console.log(changer.value)
+    //     } else if (index === 1) {
+    //         console.log(changer.value)
+    //     } else {
+    //         // percent()
+    //         console.log(changer.value)
+    //     }
+    // })
